d856f5dc55213a60a869dd5f2f016c882258f5ae
amendes96 <altonmendes8@gmail.com>
2018-01-15 16:27:52 -0500

    Added Google Sheets Reader

--- Youtube/src/test/java/testcasesyoutube/Testvalidsignin.java
+++ Youtube/src/test/java/testcasesyoutube/Testvalidsignin.java
@@ -4,11 +4,13 @@ import base.CommonAPI;
 import home.ValidSignIn;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
+import reporting.TestLogger;
 
 public class Testvalidsignin extends CommonAPI{
 
     @Test
     public void login() throws InterruptedException {
+        //TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         ValidSignIn vsn = PageFactory.initElements(driver, ValidSignIn.class);
         vsn.login();
         System.out.println(getCurrentPageUrl());

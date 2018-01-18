aa748e5ba8b19703995548c81eaa2447ebd10574
amendes96 <altonmendes8@gmail.com>
2018-01-18 15:00:18 -0500

    Sheet Reader

---
+++ Youtube/src/main/java/googlesheetspage/GoogleSheets.java
@@ -0,0 +1,39 @@
+package googlesheetspage;
+
+import base.CommonAPI;
+import com.google.api.services.sheets.v4.model.*;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import static googleApi.GoogleSheetReader.getSheetsService;
+
+public class GoogleSheets extends CommonAPI{
+
+    public List<List<Object>> getSpreadSheetRecords(String spreadsheetId, String range) throws IOException {
+        Sheets service = getSheetsService();
+        ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+        List<List<Object>> values = response.getValues();
+        if (values == null || values.size() == 0) {
+            return null;
+        } else {
+            return values;
+        }
+    }
+    public List<String> searchItemByName(String spreadsheetId, String range) throws IOException, InterruptedException {
+        List<List<Object>> values = getSpreadSheetRecords(spreadsheetId, range);
+        List<String> actual = new ArrayList<>();
+        for (List row : values) {
+            Thread.sleep(2000);
+            typeByID("search", row.get(1).toString());
+            Thread.sleep(2000);
+            clearInputbyID("search");
+            Thread.sleep(2000);
+        }
+        return actual;
+    }
+}

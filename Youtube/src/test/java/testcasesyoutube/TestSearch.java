f9356f9967fc6efdb7d8204266ceeb5223eeb8ff
amendes96 <altonmendes8@gmail.com>
2018-01-18 14:27:44 -0500

    Data Reader Error

--- Youtube/src/test/java/testcasesyoutube/TestSearch.java
+++ Youtube/src/test/java/testcasesyoutube/TestSearch.java
@@ -1,20 +1,19 @@
 package testcasesyoutube;
 
 import base.CommonAPI;
-import home.SearchClass;
+import reporting.TestLogger;
+import search.SearchClass;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 public class TestSearch extends CommonAPI{
 
     @Test
-    public void testSearch() throws InterruptedException {
+    public void testSearch() throws InterruptedException, IOException {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
         SearchClass search = PageFactory.initElements(driver, SearchClass.class);
         search.searching();
-    }
-    @Test
-    public void tstsearch()throws InterruptedException{
-        SearchClass src = PageFactory.initElements(driver,SearchClass.class);
-        src.searching();
     }
 }

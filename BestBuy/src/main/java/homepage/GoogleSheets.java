e083e6ddc4ba8a938b20f4a851dbcea58eeb8118
rafiq <sharifrafiq1@gmail.com>
2018-01-20 10:20:02 -0800

    Best Buy Added

--- BestBuy/src/main/java/homepage/GoogleSheets.java
+++ BestBuy/src/main/java/homepage/GoogleSheets.java
@@ -1,7 +1,62 @@
 package homepage;
 
+import base.CommonAPI;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static googleApi.GoogleSheetReader.getSheetsService;
+
 /**
  * Created by SharifRafiq on 1/19/2018.
  */
-public class GoogleSheets {
+public class GoogleSheets extends CommonAPI {
+    public List<List<Object>> getSpreadSheetRecords(String spreadsheetId, String range) throws IOException {
+        // Build a new authorized API client service.
+        Sheets service = getSheetsService();
+        ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+        List<List<Object>> values = response.getValues();
+        if (values == null || values.size() == 0) {
+            return null;
+        } else {
+            return values;
+        }
+    }
+
+    public List<String> searchByName(String spreadsheetId, String range) throws IOException, InterruptedException {
+//        driver.switchTo().frame(1);
+      driver.findElement(By.id("close")).click();
+        List<List<Object>> values = getSpreadSheetRecords(spreadsheetId, range);
+        List<String> actual = new ArrayList<>();
+        for (List row : values) {
+            sleepFor(1);
+            typeByIdNEnter("gh-search-input", row.get(1).toString());
+            sleepFor(1);
+            actual.add(getCurrentPageTitle());
+            clearInputFieldById("gh-search-input");
+            sleepFor(1);
+        }
+        return actual;
+    }
+    public List<String> searchItemByBarCode(String spreadsheetId, String range) throws IOException, InterruptedException {
+        List<List<Object>> values = getSpreadSheetRecords(spreadsheetId, range);
+        List<String> actual = new ArrayList<>();
+        for (List row : values) {
+            sleepFor(3);
+            typeByIdNEnter("gh-search-input", row.get(1).toString());
+            sleepFor(3);
+            actual.add(getTextByXpath("//*[text()='"+row.get(2)+"']"));
+            System.out.println(row.get(2));
+            clearInputFieldById("gh-search-input");
+            sleepFor(3);
+        }
+        return actual;
+    }
 }
--- BestBuy/src/main/java/homepage/SearchItems.java
+++ BestBuy/src/main/java/homepage/SearchItems.java
@@ -1,7 +1,50 @@
 package homepage;
 
+import base.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import reporting.TestLogger;
+
+import java.io.IOException;
+
 /**
  * Created by SharifRafiq on 1/19/2018.
  */
-public class SearchItems {
-}
+public class SearchItems extends CommonAPI {
+    @FindBy(how = How.CSS, using ="#gh-search-input")
+    public static WebElement searchInputWebElement;
+    @FindBy(how = How.CSS, using =".search-input")
+    public static WebElement submitButtonWebElement;
+
+    public static WebElement getSearchInputWebElement() {
+        return searchInputWebElement;
+    }
+    public static WebElement getSubmitButtonWebElement() {
+        return submitButtonWebElement;
+    }
+    public void searchFor(String value){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName())+" "+ value);
+        getSearchInputWebElement().sendKeys(value);
+    }
+    public void submitSearchButton(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        getSubmitButtonWebElement().click();
+    }
+    public void clearInput(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        getSearchInputWebElement().clear();
+    }
+//    public void searchItemsAndSubmitButton() throws IOException, InterruptedException {
+//        TestLogger.log(getClass().getSimpleName() + ": " + (new Object(){}.getClass().getEnclosingMethod().getName()));
+//        ItemsToBeSearched itemsToBeSearched = new ItemsToBeSearched();
+//        String [] value = itemsToBeSearched.getDataFromExcelFile();
+//        for(int i=0; i<value.length; i++) {
+//            sleepFor(1);
+//            searchFor(value[i]);
+//            sleepFor(1);
+//            submitSearchButton();
+//            sleepFor(1);
+//            clearInput();
+//        }
+    }

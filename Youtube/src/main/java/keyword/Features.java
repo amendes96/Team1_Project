36c95accfafb9a4e7c8d35f5c4d6f7ad6b505d32
amendes96 <altonmendes8@gmail.com>
2018-01-26 21:31:57 -0500

    Config Dependencies

---
+++ Youtube/src/main/java/keyword/Features.java
@@ -0,0 +1,10 @@
+package keyword;
+
+import base.CommonAPI;
+
+public class Features extends CommonAPI{
+
+//    ExcelFileReader objExcelFileReader;
+//    HomePage objHomePage;
+//    LoginPage objLoginPage;
+}
